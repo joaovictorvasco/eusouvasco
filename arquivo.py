d705c2ee4da674d1a30c6ad0ff9100f73565e65d
João Victor Sepulveda penido <joaovictor.gestaodetrafego@gmail.com>
2024-08-12 22:54:45 -0300

    ultimo passo

--- arquivo.py
+++ arquivo.py
@@ -1,5 +1,15 @@
+import random
+
 def cumprimento(texto):
     return f"Olá, {texto}"
 
 # Chamando a função com o nome completo do aluno
 print(cumprimento("Nome Completo do Aluno"))
+
+def sorteia_media():
+    numeros = [random.randint(1, 100) for _ in range(3)]
+    media = sum(numeros) / 3
+    return media
+
+# Chamando a função que sorteia 3 números aleatórios e calcula a média
+print(f"A média dos três números sorteados é: {sorteia_media()}")
